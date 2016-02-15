f5fcba32b6a51be2a3ca8457c3567f877afb56ef
Sena Yoga Samudra <sena@Senas-MacBook.local>
2016-02-15 15:24:09 +0700

    first commit

---
+++ LanguageAsset.php
@@ -0,0 +1,9 @@
+<?php
+namespace nubors\tinymce;
+
+use yii\web\AssetBundle;
+
+class LanguageAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/nubors/yii2-tinymce/langs';
+} 
---
+++ TinyMceAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace nubors\tinymce;
+
+use yii\web\AssetBundle;
+
+class TinyMceAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/tinymce/tinymce';
+    public $js = ['tinymce.jquery.min.js'];
+    public $depends = ['yii\web\JqueryAsset'];
+} 
